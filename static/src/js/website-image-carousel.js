8af74138b74df02774ae4369a6a08d2b31784f7d
Ryan Cole <mginshe@gmail.com>
2016-10-19 07:51:54 +0000

    Tweaked slider code slightly, removed container padding (slider now aligns with other website objects)

--- static/src/js/website-image-carousel.js
+++ static/src/js/website-image-carousel.js
@@ -1,9 +1,7 @@
 $(window).load(function() {
-	var numThumbnails = 5;				// number of thumbnails to show
-	var pageSize = 5;					// number of thumbnails to scroll per page
-	var autoplaySpeed = false;			// number of ms to wait between auto-scroll, or false for off
-	var numImages = $('.website-multi-image-thumbnails img').length;
-
+	var numThumbnails = 6;				// number of thumbnails to show
+	var pageSize = 6;					// number of thumbnails to scroll per page
+	var autoplaySpeed = 3000;			// number of ms to wait between auto-scroll, or false for off
 
 	$('.website-multi-image-main').slick({
 		slidesToShow: 1,
@@ -11,17 +9,18 @@ $(window).load(function() {
 		arrows: false,
 		adaptiveHeight: true,
 		autoplay: (autoplaySpeed ? true : false),
+		autoplaySpeed: autoplaySpeed,
 		pauseOnHover: true,
 		infinite: true,
    		focusOnSelect: true,
-
 
 		asNavFor: '.website-multi-image-thumbnails',
 	});
 	$('.website-multi-image-thumbnails').slick({
 		slidesToShow: numThumbnails,
 		slidesToScroll: pageSize,
-		infinite: false,
+		infinite: true,
+		centerMode: true,
 		arrows: false,
    		focusOnSelect: true,
 
@@ -30,9 +29,9 @@ $(window).load(function() {
 
 });
 $(window).resize(function() {
-  $('.js-slider').slick('resize');
+	$('.js-slider').slick('resize');
 });
 
 $(window).on('orientationchange', function() {
-  $('.js-slider').slick('resize');
+	$('.js-slider').slick('resize');
 });
