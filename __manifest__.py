6f96368aa5ecb7774e9343cfe58f386e7aa27ebb
Luke Branch <lukebranch@gmail.com>
2016-10-26 11:40:18 +0800

    Update __manifest__.py - installable = False

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name" : "Website Multi-Image",
+    "version" : "1.0",
+    "author" : "Luke Branch, Cristian Sebastian Rocha, Ryan Cole",
+    "website" : "http://github.com/OdooCommunityWidgets",
+    "license": "MIT",
+    "category" : "Added functionality",
+    "depends" : ["product","sale","website_sale"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "description": """
+    """,
+    'update_xml': [
+        'views/product_images.xml',
+        'views/website_product_image_carousel.xml',
+        'views/theme.xml',
+        'views/res_config.xml',
+        'security/ir.model.access.csv',
+    ],
+    'test':[''],
+    'installable': False,
+    'active': False,
+}
